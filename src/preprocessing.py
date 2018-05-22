567131314f35890d0109b90533c6004e901f8d5d
Nini <gamelani@ethz.ch>
2018-05-23 00:20:05 +0200

    Modified Comment

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -118,8 +118,8 @@ def pos_tagging_text(sentence):
 
 def pos_tag_dataset(dataset):
     """
-    Saves two files containing an array of size #stories/rows by 5 (story id, sen1 to sen4) for the former
-    and an array of size #stories/rows by 2 (story id, sen5).
+    Saves two files containing pos-tagged sentences.
+    Arrays of size #stories/rows by 5 (story id, sen1 to sen4) and of size #stories/rows by 2 (story id, sen5).
     Each column (not id) is a tockenized and pos-tagged sentence of the form (ie: [[Kelly, RB], [studied, VBN], [.,.]])
 
     :param dataset: dataframe containing train / val / test data
@@ -226,6 +226,12 @@ if __name__ == '__main__':
     # n_stories, *_ = x_begin.shape
     # x_begin = np.reshape(x_begin, (n_stories, -1))
     # print(x_begin.shape)
+
+    matrix = np.load(train_pos_begin)
+    for item in matrix[0:10,1]:
+        print(item[:,1])
+    # print(matrix[0,1])
+
 
     # dataset=train_set
     # pos_tag_dataset(dataset)
