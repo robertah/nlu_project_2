f0c94417a083b7a153f272a26832fbfa9aa95ebf
Nini <gamelani@ethz.ch>
2018-05-24 21:51:07 +0200

    Added function to save pos tagged story corpus - sentences 1 to 4 together

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -113,7 +113,7 @@ def pos_tagging_text(sentence):
     return nltk.pos_tag(tokens)
 
 
-def pos_tag_dataset(dataset):
+def pos_tag_dataset(dataset, seperate=False):
     """
     Saves two files containing pos-tagged sentences.
     Arrays of size #stories/rows by 5 (story id, sen1 to sen4) and of size #stories/rows by 2 (story id, sen5).
@@ -130,41 +130,78 @@ def pos_tag_dataset(dataset):
     data_original = load_data(dataset)
 
     # Creates dataframes with pos-tagged sentences
-    pos_begin = pd.DataFrame(columns=['id', 'sen1', 'sen2', 'sen3', 'sen4'])
     pos_end = pd.DataFrame(columns=['id', 'sen5'])
 
     story_number = 0
-    total_stories = len(pos_begin)
-    for index, row in data_original.iterrows():
-        pos_begin.loc[index] = [index,
-                                np.asarray(pos_tagging_text(row['sen1']), object),
-                                np.asarray(pos_tagging_text(row['sen2']), object),
-                                np.asarray(pos_tagging_text(row['sen3']), object),
-                                np.asarray(pos_tagging_text(row['sen4']), object)]
-        pos_end.loc[index] = [index, pos_tagging_text(row['sen5'])]
-        story_number = story_number + 1
-        
-        if story_number%1000 == 0:
-            print("Processed ",story_number, "/",total_stories)
+    total_stories = len(pos_end)
 
-    pos_begin = np.asarray(pos_begin)
-    pos_end = np.asarray(pos_end)
+    if seperate:
+        pos_begin = pd.DataFrame(columns=['id', 'sen1', 'sen2', 'sen3', 'sen4'])
+        pos_begin = pd.DataFrame(columns=['id', 'sen'])
+        for index, row in data_original.iterrows():
+            pos_begin.loc[index] = [index,
+                                    np.asarray(pos_tagging_text(row['sen1']), object),
+                                    np.asarray(pos_tagging_text(row['sen2']), object),
+                                    np.asarray(pos_tagging_text(row['sen3']), object),
+                                    np.asarray(pos_tagging_text(row['sen4']), object)
+                                    ]
+            pos_end.loc[index] = [index, pos_tagging_text(row['sen5'])]
+            story_number = story_number + 1
 
-    print("Saving pos tagged corpus..")
-    # Saving models in two data files
-    cur_dir = os.path.splitext(train_set)[0]
-    path_begin = cur_dir + "_pos_begin"
-    path_end = cur_dir + "_pos_end"
+            if story_number % 1000 == 0:
+                print("Processed ", story_number, "/", total_stories)
 
-    np.save(path_begin, pos_begin)
-    np.save(path_end, pos_end)
-    print("Saved the pos tagged corpus successfully !")
+        print("Saving pos tagged corpus..")
+        # Saving models in two data files
+        cur_dir = os.path.splitext(train_set)[0]
+        path_begin = cur_dir + "_pos_begin"
+        path_end = cur_dir + "_pos_end"
 
-    # To load dataset, then do np.load(train_pos_begin)
+        np.save(path_begin, pos_begin)
+        np.save(path_end, pos_end)
+        print("Saved the pos tagged corpus successfully !")
 
-    return None
+        # To load dataset, then do np.load(train_pos_begin)
 
 
+    else:
+        pos_begin = pd.DataFrame(columns=['id', 'sen'])
+        for index, row in data_original.iterrows():
+            pos_begin.loc[index] = [index,
+                                    np.concatenate((np.asarray(pos_tagging_text(row['sen1']), object),
+                                                    np.asarray(pos_tagging_text(row['sen2']), object),
+                                                    np.asarray(pos_tagging_text(row['sen3']), object),
+                                                    np.asarray(pos_tagging_text(row['sen4']), object)
+                                                    ))]
+            pos_end.loc[index] = [index, pos_tagging_text(row['sen5'])]
+            story_number = story_number + 1
+
+            if story_number % 1000 == 0:
+                print("Processed ", story_number, "/", total_stories)
+
+        pos_begin = np.asarray(pos_begin)
+        pos_end = np.asarray(pos_end)
+
+        print("Saving pos tagged corpus..")
+        # Saving models in two data files
+        cur_dir = os.path.splitext(train_set)[0]
+        path_begin = cur_dir + "_pos_begin_together"
+        path_end = cur_dir + "_pos_end_together"
+
+        np.save(path_begin, pos_begin)
+        np.save(path_end, pos_end)
+        print("Saved the pos tagged corpus successfully !")
+
+
+    return pos_begin, pos_end
+
+
+def combine_matrix_cols(array):
+
+    return combined_matrix
+
+
+# TODO Remove if not used
 def open_csv_asmatrix(datafile):
     print("Loading ", datafile)
     file_csv = pd.read_csv(datafile)
@@ -189,16 +226,64 @@ if __name__ == '__main__':
     # print(matrix[0,1])
 
 
-    # dataset=train_set
-    # pos_tag_dataset(dataset)
+    dataset=train_set
+    pos_begin, pos_end = pos_tag_dataset(dataset, seperate=False)
+
+
+    #----------
+
+    # x = [["a", "b"], ["c", ]]
+    #
+    # result = sum(x, [])
+    # print(result)
+    #
+    # matrix = np.load(train_pos_begin)
+    #
+    # print(matrix.shape)
+    # matrix = matrix[0:5,1:5]
+    #
+    #
+    # print(matrix.shape)
+    # # print(matrix[:,0])
+    # story = []
+    #
+    # new_matrix = []
+    # array_corpus = []
+    # # for i in range(0, matrix.shape[0]):
+    # i=0
+    # a = matrix[i,0]
+    # b = matrix[i,1]
+    # c = matrix[i,2]
+    # d = matrix[i,3]
+    # np.asarray(new_matrix.append(np.concatenate((a, b, c, d))))
+    # print(np.asarray([np.concatenate((a,b,c,d))], object).shape)
+    # print(new_matrix.append(np.concatenate((a,b,c,d))))
+    # print((np.asarray(new_matrix.append(np.concatenate((a,b,c,d))))))
+    # np.hstack((array_corpus, new_matrix))
+    #
+    # print(array_corpus)
+    # # print(np.concatenate(a, b), axis=1)
+    #
+    # new_matrix = [1]
+    #
+    # for index in range(0,matrix.shape[0]):
+    #     print(index)
+    #     print(matrix[index, :].tolist().shape)
+    #     np.vstack((new_matrix, matrix[index, :].tolist()))
+    # print(new_matrix)
+
+
+    #-------
+
+    # print(np.append(matrix[0:1,1], matrix[0:1,2], matrix[0:1,3], matrix[0:1,4]).shape)
 
     # sentences = _load_data(train_set)
     # print(sentences)
     # pos_text = pos_tagging_text(sentences)
     # print(pos_text)
 
-    _, data_proc = preprocess(train_set, pad='sentence')
-    print(data_proc[0])
-    n_stories, *_ = data_proc.shape
-    data_proc = np.reshape(data_proc, (n_stories*sentence_len, -1))
+    # _, data_proc = preprocess(train_set, pad='sentence')
+    # print(data_proc[0])
+    # n_stories, *_ = data_proc.shape
+    # data_proc = np.reshape(data_proc, (n_stories*sentence_len, -1))
     # word_embedding(data_proc)
