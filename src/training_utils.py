ce615cb88488d4e4bf4f872ee3adb444e6330e26
Nini <gamelani@ethz.ch>
2018-05-31 18:28:47 +0200

    Added functions batch_iter for SiameseLSTM

--- src/training_utils.py
+++ src/training_utils.py
@@ -13,7 +13,7 @@ def batch_iter(data, batch_size, num_epochs, shuffle=False):
     return
                 
 
-
+# TODO What is validation for in this function?
 def full_stories_together(contexts, endings, validation = False, list_array = False):
 
     full_stories_batches = []
@@ -142,3 +142,64 @@ def batch_iter_train_cnn(contexts, endings, neg_end_obj, out_tagged_story = Fals
             stories_batch = batches_full_stories[batch_idx]
             verifier_batch = [[int(ver), 1-int(ver)] for ver in ver_batch_end[batch_idx]]
             yield (np.asarray(stories_batch), np.asarray(verifier_batch))
+
+# --------- For Siamese LSTM --------
+
+def batch_iter_train_SiameseLSTM(contexts, endings, neg_end_obj, out_tagged_story = False,
+                         batch_size = 2, num_epochs = 500, shuffle=True):
+    '''
+    Generates a batch generator for the train set.
+    Same idea as batch_iter_train_cnn function except that returns context, ending and verifier separately
+    :param contexts: array of all context (separate or together...?) #TODO check which one applies
+    :param endings: array of all endings
+    :param neg_end_obj: array with all negative endings
+    :param out_tagged_story:
+    :param batch_size:
+    :param num_epochs:
+    :param shuffle:
+    :return:
+    '''
+
+    if not out_tagged_story:
+        contexts = eliminate_tags_in_contexts(contexts_pos_tagged= contexts)
+    while True:
+    #for i in range(0,num_epochs):
+        print("Augmenting with negative endings for the next epoch -> stochastic approach..")
+        batch_endings, ver_batch_end= batches_pos_neg_endings(neg_end_obj = neg_end_obj, data = endings,
+                                                              batch_size = batch_size)
+        batches_full_stories = np.array([contexts, batch_endings])  #full_stories_together(contexts = contexts, endings = batch_endings)
+        print("Shape of batches_full_stories: {}".format(batches_full_stories).shape)
+        print("Shape should be {} times {}".format(len(context), 2))
+
+        print("Length context: {}".format(len(contexts)))
+        total_steps = len(contexts)
+        print("Train generator for the new epoch ready..")
+
+        for batch_idx in range(0, total_steps):
+            #batch_size stories -> 1 positive endings + batch_size-1 negative endings ones
+
+            stories_batch = batches_full_stories[batch_idx] # TODO Change batches_full_stories
+            verifier_batch = [[int(ver), 1-int(ver)] for ver in ver_batch_end[batch_idx]]
+            yield (np.asarray(stories_batch), np.asarray(verifier_batch))
+
+
+def batch_iter_val_SiameseLSTM(contexts, endings, neg_end_obj, binary_verifiers, out_tagged_story = False,
+                       batch_size = 2, num_epochs = 500, shuffle=True):
+    """
+    Generates a batch generator for the validation set.
+    """
+    if not out_tagged_story:
+        contexts = eliminate_tags_in_contexts(contexts_pos_tagged = contexts)
+        endings = eliminate_tags_in_val_endings(endings_pos_tagged = endings)
+
+    while True:
+
+        batches_full_stories = np.array([contexts, batch_endings])#full_stories_together(contexts = contexts, endings = endings, validation = True)
+
+        total_steps = len(batches_full_stories)
+
+        for batch_idx in range(0, total_steps):
+            #batch_size stories -> 1 positive endings + batch_size-1 negative endings ones
+            stories_batch = batches_full_stories[batch_idx]
+            binary_batch_verifier = [[int(ver), 1-int(ver)] for ver in binary_verifiers[batch_idx]]
+            yield (np.asarray(stories_batch), np.asarray(binary_batch_verifier))
